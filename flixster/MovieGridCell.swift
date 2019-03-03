dd689bccf55ae88971ecb1865c7e5ac28a240340
JShimonov <jshimonov@gmail.com>
2019-03-03 02:58:28 -0500

    Put in the tab bar

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Joseph Shimonov on 3/3/19.
+//  Copyright © 2019 Joseph Shimonov. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
